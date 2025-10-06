de3ad21592ff59ae57f918f4a1b27c3925881564
sb <rambhatat@gmail.com>
2025-10-05 20:06:13 -0500

    Add Pages workflow for dev branch

--- scope/vite.config.ts
+++ scope/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/scope-simulator/',
   test: {
     globals: true,
     environment: 'jsdom',
